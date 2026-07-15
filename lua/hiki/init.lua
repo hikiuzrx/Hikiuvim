245c15249ed7f690e79c55c0db9e6c06153f3b16
hikiuzrx <zch92142@gmail.com>
2026-07-15 17:09:30 +0100

    feat: add options.lua with core editor settings

--- lua/hiki/init.lua
+++ lua/hiki/init.lua
@@ -1,3 +1,4 @@
+require("hiki.options")
 require("hiki.lazy")
 require("hiki.remap")
 require("hiki.colors")
---
+++ lua/hiki/options.lua
@@ -0,0 +1,30 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tabs & indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- No line wrap
+vim.opt.wrap = false
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Appearance
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+-- Behavior
+vim.opt.updatetime = 50
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
